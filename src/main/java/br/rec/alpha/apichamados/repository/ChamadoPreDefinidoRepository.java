ffa66d5e2dd31f45d073d96dee16138953c3400f
silva <silva@192.168.0.119>
2020-03-12 11:11:16 -0300

    Alterando senha do banco de dados

--- src/main/java/br/rec/alpha/apichamados/repository/ChamadoPreDefinidoRepository.java
+++ src/main/java/br/rec/alpha/apichamados/repository/ChamadoPreDefinidoRepository.java
@@ -3,7 +3,9 @@ package br.rec.alpha.apichamados.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.rec.alpha.apichamados.model.ChamadoPreDefinido;
+
 @Repository
-public interface ChamadoPreDefinidoRepository extends JpaRepository<ChamadoPreDefinidoRepository, Long>{
+public interface ChamadoPreDefinidoRepository extends JpaRepository<ChamadoPreDefinido, Long>{
 
 }
