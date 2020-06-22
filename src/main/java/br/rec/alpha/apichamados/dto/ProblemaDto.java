4854c53f2e648a0ff4a52b9bd61e9e7b76820d63
Matheus Monteiro <silvamm.matheus@gmail.com>
2020-06-21 21:45:11 -0300

    Correções

--- src/main/java/br/rec/alpha/apichamados/dto/ProblemaDto.java
+++ src/main/java/br/rec/alpha/apichamados/dto/ProblemaDto.java
@@ -1,17 +1,11 @@
 package br.rec.alpha.apichamados.dto;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import br.rec.alpha.apichamados.model.Problema;
 import lombok.Data;
 
 @Data
 public class ProblemaDto {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	
